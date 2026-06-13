c7e86ec2423625fcbabf201a5f0f376e24cff3b4
AzureTear <ba.maiolatesi@duocuc.cl>
2026-06-13 19:29:49 -0400

    arreglo brevo SDK v2 import

--- src/emails/mailer.js
+++ src/emails/mailer.js
@@ -1,7 +1,6 @@
 const Brevo = require('@getbrevo/brevo');
 
-const client = Brevo.ApiClient.instance;
-client.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
+const apiInstance = new Brevo.TransactionalEmailsApi();
+apiInstance.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
 
-const mailer = new Brevo.TransactionalEmailsApi();
-module.exports = mailer;
+module.exports = apiInstance;
